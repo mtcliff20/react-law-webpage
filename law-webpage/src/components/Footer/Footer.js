607d375814ee06c1b3ad322a1c773d37bea4362a
Matthew Clifford <mtclifford12@gmail.com>
2022-04-26 22:26:51 -0500

    Fix footer

--- law-webpage/src/components/Footer/Footer.js
+++ law-webpage/src/components/Footer/Footer.js
@@ -5,27 +5,16 @@ import { Link } from 'react-router-dom';
 function Footer() {
   return (
     <div className='footer-container'>
-      <section className='footer-subscription'>
-        <p className='footer-subscription-heading'>
-          Contact Us
-        </p>
-        <p className='footer-subscription-text'>
-          120 West Madison, Suite 1407, Chicago, IL, 60602
-        </p>
-        <p className='footer-subscription-text'>
-        t.312.213.4707
-        </p>
-      </section>
-      <div class='footer-links'>
+      <div className='footer-links'>
         <div className='footer-link-wrapper'>
-          <div class='footer-link-items'>
+          <div className='footer-link-items'>
             <h2>About Us</h2>
             <Link to='/'>Testimonials</Link>
             <Link to='/career'>Careers</Link>
             <Link to='/'>Investors</Link>
             <Link to='/'>Terms of Service</Link>
           </div>
-          <div class='footer-link-items'>
+          <div className='footer-link-items'>
             <h2>Contact Us</h2>
             <Link to='/contact'>Contact</Link>
             <Link to='/contact'>Support</Link>
@@ -34,7 +23,7 @@ function Footer() {
           </div>
         </div>
         <div className='footer-link-wrapper'>
-          <div class='footer-link-items'>
+          <div className='footer-link-items'>
             <h2>Social Media</h2>
             <Link to='/'>Instagram</Link>
             <Link to='/'>Facebook</Link>
@@ -43,59 +32,54 @@ function Footer() {
           </div>
         </div>
       </div>
-      <section class='social-media'>
-        <div class='social-media-wrap'>
-          <div class='footer-logo'>
-            <Link to='/' className='social-logo'>
-              Frenzel Law LLC
-              <i class='fab fa-typo3' />
-            </Link>
-          </div>
-          <small class='website-rights'>Frenzel Law LLC © 2022</small>
-          <div class='social-icons'>
+      <section className='social-media'>
+        <div className='social-media-wrap'>
+          <div className='social-icons'>
             <Link
-              class='social-icon-link facebook'
+              className='social-icon-link facebook'
               to='/'
               target='_blank'
               aria-label='Facebook'
             >
-              <i class='fab fa-facebook-f' />
+              <i className='fab fa-facebook-f' />
             </Link>
             <Link
-              class='social-icon-link instagram'
+              className='social-icon-link instagram'
               to='/'
               target='_blank'
               aria-label='Instagram'
             >
-              <i class='fab fa-instagram' />
+              <i className='fab fa-instagram' />
             </Link>
             <Link
-              class='social-icon-link youtube'
+              className='social-icon-link youtube'
               to='/'
               target='_blank'
               aria-label='Youtube'
             >
-              <i class='fab fa-youtube' />
+              <i className='fab fa-youtube' />
             </Link>
             <Link
-              class='social-icon-link twitter'
+              className='social-icon-link twitter'
               to='/'
               target='_blank'
               aria-label='Twitter'
             >
-              <i class='fab fa-twitter' />
+              <i className='fab fa-twitter' />
             </Link>
             <Link
-              class='social-icon-link twitter'
+              className='social-icon-link twitter'
               to='/'
               target='_blank'
               aria-label='LinkedIn'
             >
-              <i class='fab fa-linkedin' />
+              <i className='fab fa-linkedin' />
             </Link>
           </div>
         </div>
       </section>
+    <p>120 West Madison, Suite 1407, Chicago, IL, 60602</p>
+    <p>t.312.213.4707</p>
     </div>
   );
 }
