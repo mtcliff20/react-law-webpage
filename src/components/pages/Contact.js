3c25b4bce172258c31a2e24032a0bed59cb45d96
Matthew Clifford <mtclifford12@gmail.com>
2022-05-03 20:17:12 -0500

    Fix paragraph size

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -5,7 +5,8 @@ export default function Contact(){
     return <div className='contact'>
     <h2>Contact</h2>
     <p>Frenzel Law's Real Estate Department has a leading practice focused of aquisitions 
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi sollicitudin nisi in libero faucibus, et ullamcorper dolor venenatis. Sed quis sem rhoncus, volutpat ante a, rhoncus ante. Morbi lacinia justo vitae dictum sagittis. Suspendisse potenti. Suspendisse ut mi in nisl posuere bibendum vitae sit amet risus. In ultricies venenatis lacus ac sagittis. Nam finibus leo lectus, at placerat lacus tempor vel. Etiam sed euismod lectus, sit amet semper nisl. Mauris dapibus tristique nisi, id ultricies nisl rhoncus nec. Quisque at eros congue,
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed quis sem rhoncus, volutpat ante a, rhoncus ante. Morbi lacinia justo vitae dictum sagittis. 
+    In ultricies venenatis lacus ac sagittis. Nam finibus leo lectus, at placerat lacus tempor vel. Etiam sed euismod lectus, sit amet semper nisl. Mauris dapibus tristique nisi, id ultricies nisl rhoncus nec. Quisque at eros congue,
     commodo nisl eget, elementum odio. Nam id lacinia elit. Praesent feugiat arcu eros, non faucibus nibh laoreet sit amet. Aenean elementum vel neque a hendrerit.
     </p>
     <span></span>
